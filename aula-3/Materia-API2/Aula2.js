8c045cb1df26116c937a1b9bbd37b1e37637c1fd
Guilherme-Lyra <guilyra99@gmail.com>
2025-08-21 21:45:46 -0300

    Add files via upload

---
+++ aula-3/Materia-API2/Aula2.js
@@ -0,0 +1,116 @@
+// Declarando variaveis
+/* var a = "Guilherme";// variavel de escopo global 
+let b = 30; // Variavel de escopo locaL só fica dentro da função e ocupa menos memoria 
+const texto = "Olá"; // sempre terá o mesmo valor enquanto o programa rodar
+
+
+console.log(a); */
+
+
+
+/*funções  */
+
+/* function soma (a,b){
+    console.log(a) 
+    return a+b
+}
+
+console.log(soma(7,3)) 
+
+var teste = [1,2,3]
+/*exibindo toda a lista*/
+
+/* console.log(teste[0])
+// exibindo um elemento por posição
+console.log(teste[1])
+console.log(teste[2])
+console.log(teste[3])
+
+teste.push(4) // adiciona um valor na lista da variavel 
+console.log(teste) */
+
+
+/* var aluno = [
+{
+    nome: "Guilherme",
+    aprovado:true
+},
+{
+nome:"fulano",
+aprovado:false
+},
+{
+    nome:"teste",
+    aprovado:false
+}
+
+]
+
+console.log (aluno)
+console.log (aluno[0])
+console.log (aluno[0].nome)
+console.log (aluno[0].aprovado) */
+
+/*console.log(aluno.nome)*/
+
+/* function somaUm(x,n){
+    return x+n
+}
+
+const somaDois = (a,b) =>{
+    return a+b
+}
+
+console.log(somaUm(5,6))
+console.log(somaDois(7,6)) */
+
+/*var a = 5 
+var b = 9 
+
+function soma(a,b){
+    return a+b
+}
+
+function subtracao(a,b){
+    return a-b
+}
+const multiplicacao = (a,b) =>{
+    return a*b
+}
+const  divisao = (a,b) =>{
+    return a/b
+}
+
+console.log(soma(a,b))
+console.log(subtracao(a,b))
+console.log(multiplicacao(a,b))
+console.log(divisao(a,b)) */
+
+var carro = [
+    {
+    modelo: "Palio",
+    ano: 1998,
+    cor: "Preto"
+    },
+
+    {  
+    modelo: "vectra",
+    ano: 2000,
+    cor: "azul"     
+    },
+
+    {
+    modelo: "monza",
+    ano: 2003,
+    cor: "Preto"
+    }
+]
+
+console.log(carro)
+console.log(carro[0])
+console.log(carro[0].modelo)
+
+
+
+
+
